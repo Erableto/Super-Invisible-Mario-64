24729078ddd5c6e63c583b0f3053e2ae96f94ecf
Erableto <alberto_jurado_roldan@outlook.com>
2026-09-25 22:15:09 +0200

    Changed internal ROM name

--- asm/rom_header.s
+++ asm/rom_header.s
@@ -14,11 +14,7 @@
 .word  0x00000000               /* Checksum 2 */
 .word  0x00000000               /* Unknown */
 .word  0x00000000               /* Unknown */
-#ifdef VERSION_SH
-.ascii "SUPERMARIO64        "   /* Internal ROM name */
-#else
-.ascii "SUPER MARIO 64      "   /* Internal ROM name */
-#endif
+.ascii "INVISIBLE SM64      "   /* Internal ROM name */
 .word  0x00000000               /* Unknown */
 .word  0x0000004E               /* Cartridge */
 .ascii "SM"                     /* Cartridge ID */
